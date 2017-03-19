222957a243f72bca3d3af48daff9700ec3262e76
sachip <sachip@V-TESONJ-DEV>
2017-03-19 09:24:35 -0700

    create account

---
+++ BedrockBank/Account.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BedrockBank
+{
+    class Account
+    {
+        #region Properties
+        /// <summary>
+        /// Account number
+        /// </summary>
+        public int AccountNumber { get; }
+
+        public string EmailAddress { get; set; }
+
+        public decimal Balance { get; private set; }
+
+        public string TypeOfAccount { get; set; }
+
+        #endregion
+
+        #region Methods
+
+        public void Deposit(decimal amount)
+        {
+            Balance+= amount;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            Balance -= amount;
+        }
+        #endregion 
+    }
+}
---
+++ BedrockBank/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BedrockBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account1 = new Account();
+            account1.EmailAddress = "test@test.com";
+            account1.TypeOfAccount = "Checking";
+            account1.Deposit(100.40M);
+            Console.WriteLine($"AccountNumber: {account1.AccountNumber}, TypeOfAccount: {account1.TypeOfAccount}, Balance: {account1.Balance}");
+            Console.ReadLine();
+
+        }
+    }
+}
